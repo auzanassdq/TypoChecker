ee5d2cfe09139ae37defcfd5195304d814d449ed
Auzan <auzanassidqi@gmail.com>
2019-01-12 18:53:46 +0700

    add IndoWordList

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,14 @@
 package com.company;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        String[] Database = {"dunia", "ini", "indah", "yang", "bebas", "luas"};
         ArrayList<Kata> wordList = new ArrayList();
+        ArrayList<String> kamusList = new ArrayList();
 
         Scanner in = new Scanner(System.in);
 
@@ -23,35 +21,53 @@ public class Main {
         try {
             // Buat object file dgn nama yg sudah di input
             File fileIn = new File(namaFileInput);
-            Scanner reader = new Scanner(fileIn);
+//            InputStream fileIn = new FileInputStream(namaFileInput);
+            InputStream kamus = new FileInputStream("IndoWordList.txt");
+
+            Scanner readFileIn = new Scanner(fileIn);
+            Scanner readKamus = new Scanner(kamus);
+
             PrintWriter writer = new PrintWriter(namaFileOutput);
 
-            reader.useDelimiter(" ");
-            while (reader.hasNext()){
-                String perKata = reader.next();
+            // Scann File Input
+            readFileIn.useDelimiter("(\\p{javaWhitespace}|\\.|,)+");
+            while (readFileIn.hasNext()){
+                String perKata = readFileIn.next();
 
                 Kata kata = new Kata(perKata);
                 wordList.add(kata);
             }
 
+            // Scann Kamus
+            readKamus.useDelimiter("");
+            while (readKamus.hasNext()){
+                String kataKamus = readKamus.nextLine();
+
+                kamusList.add(kataKamus);
+            }
+
+            // Pengecekan pada file
             for(Kata kata : wordList) {
-                for (String db : Database ) {
+                for (String db : kamusList ) {
                     if (kata.getKata().equals(db)) {
                         kata.setHasNoPair(false);
                     }
                 }
 
                 if (kata.isHasNoPair()) {
-                    writer.printf("/%s/ ", kata.getKata());
+                    writer.printf("*%s* ", kata.getKata());
                 } else {
                     writer.printf("%s ", kata.getKata());
                 }
             }
 
-            reader.close();
+            readFileIn.close();
+            readKamus.close();
             writer.close();
-        } catch (FileNotFoundException e) {
+
+        } catch (Exception e) {
             System.out.print("File tidak ditemukan..");
         }
     }
+
 }
